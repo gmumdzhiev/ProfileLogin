9d867f0e8872552164ab44af849ea86e47709815
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2018-05-30 13:01:46 +0200

    small adjustments to the App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
-/*import LoginForm from './LoginForm';*/
-/*import RegForm from './RegForm';*/
+import LoginForm from './LoginForm';
+import RegForm from './RegForm';
 
 class App extends Component {
   render() {
-
-
-    <div className='App'>
-      <LoginForm />
-      <RegForm />
-    </div>
-      );
-  } /*else {
+    const {
+      isLoggedOn,
+      username,
+    } = this.props
+    if (isLoggedOn) {
       return (
-         <LoginForm />
-      )*/
+        <div className="App">
+          Hello {username}
+        </div>
+      );
+    } else {
+      return (
+        <LoginForm />
+      )
+    }
+  }
 }
-}
-//}
-/*const mapStateToProps = (state) => ({
+const mapStateToProps = (state) => ({
   username: state.user.username,
   isLoggedOn: state.user.isLoggedOn,
-})*/
-
-
-
-
-export default connect(/*mapStateToProps*/)(App);
+})
+export default connect(mapStateToProps)(App);
