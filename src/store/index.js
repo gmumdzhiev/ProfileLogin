1cfa3c737fea8ae42a5deb4358153c6a4694362e
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2018-06-04 10:16:11 +0200

    fixing input field

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createBrowserHistory, routerReducer, routerMiddleware, startListener } from 'redux-first-routing'
 import { reducer as userReducer } from './user';
+import { reducer as formReducer } from 'redux-form'
 
 const history = createBrowserHistory()
 const reducer = combineReducers({
   router: routerReducer,
   user: userReducer,
+  form: formReducer,
 })
 const middleware = routerMiddleware(history)
 const store = createStore(reducer, composeWithDevTools(
