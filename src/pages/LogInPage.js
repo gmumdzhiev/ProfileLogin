7df72a5893bb1eda7dc98a0319536eaff08c2465
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2018-06-11 10:02:00 +0200

    installing thunk

--- src/pages/LogInPage.js
+++ src/pages/LogInPage.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import LoginForm from '../components/LoginForm'
 
-const LoginPage = () => <p>the login page</p>
+const LoginPage = () => (
+  <div>
+    <h1>Login</h1>
+    <p>Some more login informqtion</p>
+    <LoginForm />
+  </div>
+)
 
 export default LoginPage
--- src/store/index.js
+++ src/store/index.js
@@ -3,29 +3,21 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { createBrowserHistory, routerReducer, routerMiddleware, startListener } from 'redux-first-routing'
 import { reducer as userReducer } from './user';
 import { reducer as formReducer } from 'redux-form'
+import thunk from 'redux-thunk';
 
 const history = createBrowserHistory()
+
 const reducer = combineReducers({
   router: routerReducer,
   user: userReducer,
   form: formReducer,
 })
-const middleware = routerMiddleware(history)
+
 const store = createStore(reducer, composeWithDevTools(
-  applyMiddleware(middleware),
+  applyMiddleware(thunk, routerMiddleware(history)),
   // other store enhancers if any
 ));
+
 startListener(history, store)
+
 export default store
-
-
-
-
-
-
-
-
-
-
-
-
