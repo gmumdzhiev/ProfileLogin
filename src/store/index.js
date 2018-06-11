f7d0f8d61995a2f117c6da6a3a03ffd8be8ab07d
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2018-06-11 13:43:42 +0200

    adding the websocket to my store

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,7 @@
+
+
+
+
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createBrowserHistory, routerReducer, routerMiddleware, startListener } from 'redux-first-routing'
@@ -15,7 +19,7 @@ const reducer = combineReducers({
 
 const store = createStore(reducer, composeWithDevTools(
   applyMiddleware(thunk, routerMiddleware(history)),
-  // other store enhancers if any
+
 ));
 
 startListener(history, store)
---
+++ src/store/messages.js
@@ -0,0 +1,20 @@
+import { message, send, open, close } from './websocket';
+
+const initialState = {
+  log: []
+}
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case open:
+      return { log: [...state.log, 'websocket connected'] }
+    case send:
+      return { log: [...state.log, 'sent:' + action.payload] }
+    case message:
+      return { log: [...state.log, 'recieved:' + action.payload] }
+    case close:
+      return { log: [...state.log, 'websocket disconnected'] }
+    default:
+      return state
+  }
+}
