59d9aef94ecfd79f751201ad272476aa90b3c86f
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2018-06-14 11:34:24 +0200

    adding git icon

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react';
-
+import github from '../img/if_github_291716.svg'
 const Footer = () => (
   <div id='footer'>
+    <img href='https://github.com/pandamastr' id='github' src={github} alt='github.icon' />
     <p>For contacting us : +32/888 696 888</p>
     <p> Or email us : george.mumdzhiev@gmail.com</p>
 
