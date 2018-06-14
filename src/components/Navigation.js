923538c4b327d03f3174d06633ff9a58e84cb739
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2018-06-14 15:52:24 +0200

    adding some icons and styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from './Link'
-
+import logo from '../img/bear.svg'
+import chat from '../img/if_chat-70px_510868.svg'
 const Navigation = () => (
   <nav>
     <Link to="/">Home</Link>
@@ -9,6 +10,8 @@ const Navigation = () => (
     {' '}
     <Link to="/contact">Contact</Link>
     {' '}
+    <img id='logo' src={logo} alt='bear.icon' />
+    <img id='chat' src={chat} alt='chat.icon' />
     <div id='LogReg'>
       <Link to="/login">Login</Link>
       {' '}
--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import github from '../img/if_github_291716.svg'
+import linkd from '../img/if_linked_in_online_social_media_734393.svg'
 const Footer = () => (
   <div id='footer'>
-    <img href='https://github.com/pandamastr' id='github' src={github} alt='github.icon' />
+
+    <a class='link' target='_blank' rel='noopener noreferrer' href='https://linkedin.com/in/georgi-mumdzhiev-7842b814a'>
+      <img id='linkd' src={linkd} alt='linkd.icon' />
+    </a>
+
+    <a class='git' target='_blank' rel='noopener noreferrer' href='https://github.com/pandamastr'>
+      <img id='github' src={github} alt='github.icon' />
+    </a>
     <p>For contacting us : +32/888 696 888</p>
-    <p> Or email us : george.mumdzhiev@gmail.com</p>
+    <a href="mailto:george.mumdzhiev@gmail.com">Email Us</a>
 
   </div>
 )
