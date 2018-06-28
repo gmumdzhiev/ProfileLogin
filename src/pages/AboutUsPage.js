8ae5c13ea999b693049dc308a784bbde878b51b5
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2018-06-28 15:28:25 +0200

    adding text to the about page

--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -4,14 +4,26 @@ export class About extends React.Component {
   render() {
     return (
       <div>
-        <p>Geachte user,
-        Om te beginnen zal ik wat meer over mezelf vertellen.Al sinds k klein was had ik veel pass in de creative sector . Ik was al de tijd bezig met schilderen,ontwerpen,vormgeven en programmeren . Ik volgde verschillende cursusen om mijn skills te verbeteren.
+        <h1>NL</h1>
+        <p>Geachte User,<br /><br />
+          Om te beginnen zal ik wat meer over mezelf vertellen.Al sinds ik klein was had ik veel passie in de creative sector . Ik was al de tijd bezig met schilderen,ontwerpen,vormgeven en programmeren . Ik volgde verschillende cursusen om mijn skills te verbeteren.
 
-        Enekele jaren geleden heb ik mijn diploma "Grafisch ontwerper met Engles" behaald. Ik zit momenteel in de laatste fase van mijn opleiding - WebDesign.
+          Enekele jaren geleden heb ik mijn diploma "Grafisch ontwerper met Engles" behaald. Ik zit momenteel in de laatste fase van mijn opleiding - WebDesign.
 
-        Ik ben een ambitieus persoon die goed zelfstandig kan werken . En naast het feit dat ik creatief ben , heb ook een oog voor detail .
+          Ik ben een ambitieus persoon die goed zelfstandig kan werken . En naast het feit dat ik creatief ben , heb ook een oog voor detail .
          </p>
-      </div>
+        <hr />
+        <h1>EN</h1>
+        <p>
+          Hello User,<br /><br />
+          First of all i'am going to tell you something about me. Since i was a child i had a passion for the creativity sector . I was always busy with painting , designing and programming . I've subscribed for different cursues to establish my skills.A few years ago i got my degree 'Graphic design with English'. At the moment i'am in the last phase of my studying - WebDesign. I'am an ambitious person who can work in team or alone . An beside of that i'am also creative and i have an eye for details.
+          </p>
+        <hr />
+        <p> For more information download my resume.</p>
+        <a download href="/download/showme.jpg">Download</a>
+      </div >
     );
   }
 }
+
+export default About
