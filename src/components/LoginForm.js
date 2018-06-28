469d4c7e3a2d90466fafcbaccbf6e692e7042600
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2018-06-28 15:29:06 +0200

    cleaning not needed stuff

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form';
 import { send } from '../store/websocket'
+
 const LoginForm = props => {
   const { handleSubmit } = props;
   return (
