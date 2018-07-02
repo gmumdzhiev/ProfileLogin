f1d6866e5295019bae283fd508efaa6bbe38370e
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2018-07-02 13:38:16 +0200

    work in progress Google API

--- src/pages/ContactUsPage.js
+++ src/pages/ContactUsPage.js
@@ -1,6 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import GoogleMapReact from 'react-google-maps';
 
-export class Contact extends React.Component {
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: process.env.REACT_APP_MAP_KEY }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text={'Kreyser Avrora'}
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default SimpleMap;
+/*export class Contact extends React.Component {
   render() {
     return (
       <div>
@@ -10,4 +43,4 @@ export class Contact extends React.Component {
     );
   }
 }
-export default Contact
+export default Contact */
