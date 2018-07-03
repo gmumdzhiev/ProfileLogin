c3c1e4a392f0a0c505366472c219836ef11fd605
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2018-07-03 13:29:27 +0200

    replace Map components in a different file

--- src/pages/ContactUsPage.js
+++ src/pages/ContactUsPage.js
@@ -1,46 +1,14 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
-
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-class SimpleMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33
-    },
-    zoom: 11
-  };
-
-  render() {
-    return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.REACT_APP_MAP_KEY }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text={'Kreyser Avrora'}
-          />
-        </GoogleMapReact>
-      </div>
-    );
-  }
-}
-
-export default SimpleMap;
-/*export class Contact extends React.Component {
+import React from 'react';
+import Map from '../components/Map';
+export class Contact extends React.Component {
   render() {
     return (
       <div>
-        <h1>This is the Contact Page </h1>
+        <h1>Reach me out via: </h1>
 
+        <Map />
       </div>
     );
   }
 }
-export default Contact */
+export default Contact 
