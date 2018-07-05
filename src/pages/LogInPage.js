5f3a47de46ab52c470d2a18913cf9439941f51bd
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2018-07-05 15:47:34 +0200

    added logo to welcome page

--- src/pages/LogInPage.js
+++ src/pages/LogInPage.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import LoginForm from '../components/LoginForm'
 import { connect } from 'react-redux'
+
 import userIcon from '../img/user.png'
+import logo from '../img/bear.svg'
 
 const LoginPage = ({ dispatch }) => (
   <div>
-    <h1> Give us your username: <img src={userIcon} alt='user.icon' /></h1>
+    <img id='logo1' src={logo} alt='bear.icon' />
+
+    <h1>Welcome to PandaText</h1>
+    <p> Fill in your username: <img src={userIcon} alt='user.icon' /></p>
+
     <LoginForm />
-
-
   </div >
 )
 
