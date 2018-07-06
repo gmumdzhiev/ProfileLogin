0b1663d4e1f2d0566f84e6858b07fc26de4d38da
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2018-07-06 10:45:47 +0200

    creating and importing Logout button in the Nav bar

---
+++ src/components/LogoutButton.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { disconnect } from '../store/websocket'
+
+const LogoutButton = ({ dispatch }) => (
+  <button onClick={() => { dispatch({ type: disconnect }) }}>
+    LOGOUT
+    </button >
+)
+
+const mapStateToProps = (state) => ({
+  connected: state.connection.connected,
+})
+
+export default connect(mapStateToProps)(LogoutButton)
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,21 +2,24 @@ import React from 'react'
 import Link from './Link'
 import logo from '../img/bear.svg'
 import chat from '../img/if_chat-70px_510868.svg'
+import Logout from '../components/LogoutButton'
 const Navigation = () => (
   <nav>
     <Link to="/">Home</Link>
+    {' '}
+    <Link to="/websocket">Messenger</Link>
     {' '}
     <Link to="/about">About</Link>
     {' '}
     <Link to="/contact">Contact</Link>
     {' '}
+    {Logout}
     <img id='logo' src={logo} alt='bear.icon' />
     <img id='chat' src={chat} alt='chat.icon' />
-    <div id='LogReg'>
-      <Link to="/login">Login</Link>
-      {' '}
 
-    </div>
+    {' '}
+
+
   </nav>
 )
 export default Navigation
