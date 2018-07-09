2f789d7a80231c15befd20cb8e246aa443955e13
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2018-07-09 15:25:24 +0200

    still in progress - faded transitions on pages

--- src/App.js
+++ src/App.js
@@ -7,16 +7,39 @@ import Websocket from './pages/Websocket'
 import LoginPage from './pages/LogInPage';
 import ContactPage from './pages/ContactUsPage';
 import AboutPage from './pages/AboutUsPage';
-import Router from './components/Router'
-import Home from './pages/Homepage'
+import Router from './components/Router';
+import Home from './pages/Homepage';
+import Fade from './Fade';
 
 
 const NotFound = () => <h2>Error! 404</h2>
 
+const routes = {
+  '/': Home,
+  '/contact': ContactPage,
+  '/about': AboutPage,
+  '/websocket': Websocket,
+  'error': NotFound,
+}
+
 class App extends Component {
+  constructor() {
+    super()
+    this.state = { in: true }
+  }
+
+  componentDidMount() {
+    const f = () => {
+      this.setState({ in: false })
+    }
+    setTimeout(f, 2000)
+  }
+
   render() {
     const {
       connected,
+      location,
+      oldlocation,
       loggedOn,
 
     } = this.props
@@ -31,6 +54,7 @@ class App extends Component {
       return (
         <div>
           <LoginPage />
+          <Fade in={this.state.in} />
           <Footer />
         </div>
       )
@@ -42,13 +66,8 @@ class App extends Component {
 
         <Footer />
         <hr />
-        <Router routes={{
-          '/': Home,
-          '/contact': ContactPage,
-          '/about': AboutPage,
-          '/websocket': Websocket,
-          'error': NotFound,
-        }} />
+        <Router location={oldlocation} routes={routes} />
+        <Router location={location} routes={routes} />
       </div>
     );
   }
@@ -57,6 +76,8 @@ class App extends Component {
 const mapStateToProps = (state) => ({
   connected: state.connection.connected,
   loggedOn: state.connection.name !== null,
+  location: state.router.location,
+  previousLocation: state.location,
 })
 
 export default connect(mapStateToProps)(App);
